cb11929b2be29595240097cf78a0bd56428ab034
Nicholas <nics-lohr@gmx.de>
2020-03-25 21:23:04 +0100

    Added unit tests for configuration.

---
+++ tests/config.py
@@ -0,0 +1,19 @@
+from chatbot.config import Config
+from unittest import TestCase
+from pathlib import Path
+from os import environ
+
+
+class TestConfig(TestCase):
+    def setUp(self) -> None:
+        for i in environ:
+            del environ[i]
+        test_data = Path(__file__).parent / "test_data"
+        self.config = Config(test_data / "configurations", test_data / "env-file")
+
+    def test_normal(self):
+        self.assertEqual(self.config["connection"]["path"], "/websocket")
+
+    def test_hidden(self):
+        self.assertEqual(self.config["user"]["username"], "Testy")
+        self.assertEqual(self.config["user"]["password"], "Safe")
