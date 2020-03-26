dcd4ceee914c5e8ccfeb558f488df551e5ddd9fd
Nicholas <nics-lohr@gmx.de>
2020-03-26 21:16:58 +0100

    Based chat in- and output on queues.

--- chatbot/connection/chat.py
+++ chatbot/connection/chat.py
@@ -16,19 +16,41 @@ class Chat:
     def __init__(self):
         self.channels = dict()
         self.listener_tasks = dict()
-        self.msg_queue = Queue()
+        self.in_queue = Queue()
+        self.out_queue = Queue()
+
+    async def read_msg(self):
+        return await self.in_queue.get()
 
     async def handle_msg(self, msg):
-        await self.msg_queue.put(msg)
+        await self.in_queue.put(msg)
 
-    def send_msg(self, message, channel=None):
-        if channel is None:
-            channel = message.get("channel")
+    async def send_worker(self):
+        try:
+            while True:
+                msg = await self.out_queue.get()
+                self._send_msg(msg)
+        except CancelledError:
+            raise
 
+    def _send_msg(self, message):
+        channel = message["channel"]
         if channel not in self.channels:
             raise KeyError(f"Not listening to channel {channel}.")
 
         return create_task(self.channels[channel].send_msg(message))
+
+    def send_msg(self, message, channel=None):
+        if channel is not None:
+            message["channel"] = channel
+
+        self.out_queue.put_nowait(message)
+
+    async def send_msg_async(self, message, channel=None):
+        if channel is not None:
+            message["channel"] = channel
+
+            await self.out_queue.put(message)
 
     async def listen(self, channel):
         logging.info(f"Registering channel {channel}.")
