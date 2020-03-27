c71e5cda86ab7a8890209c7451691be28d37583a
Nicholas <nics-lohr@gmx.de>
2020-03-27 14:31:46 +0100

    Added negative numbers to parsing.

--- chatbot/bots/utils/parsing/common.py
+++ chatbot/bots/utils/parsing/common.py
@@ -4,7 +4,7 @@ from pyparsing import pyparsing_common as ppc
 quoted_string = pp.dblQuotedString | pp.sglQuotedString
 
 common_parsers = {
-    int: pp.Word(pp.nums, asKeyword=True).setParseAction(ppc.convertToInteger),
+    int: pp.Word('-' + pp.nums, bodyChars=pp.nums, asKeyword=True).setParseAction(ppc.convertToInteger),
     str: quoted_string,
     bool: pp.Empty().setParseAction(lambda x: True)
 }
