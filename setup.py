8ee02dcb93a67d14dd40bf1bbffbb252c3ae05c2
Nicholas <nics-lohr@gmx.de>
2020-03-27 20:41:04 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Chatbot',
+    version='',
+    packages=find_packages(),
+    url='https://github.com/Nicholas42/Chatbot',
+    license='GNU GPLv3',
+    author='Nicholas Schwab',
+    author_email='chatbot@nicholas-schwab.de',
+    description='A chatbot for the QED-Chat.'
+)
