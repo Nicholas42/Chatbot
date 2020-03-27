576af6c3745a39a1750d6ac39f8fca6a2594e8c7
Nicholas <nics-lohr@gmx.de>
2020-03-27 18:01:36 +0100

    Fixed some problems due to not copying parsers. Improved word recognition.

--- chatbot/bots/utils/parsing/common.py
+++ chatbot/bots/utils/parsing/common.py
@@ -1,8 +1,7 @@
 import pyparsing as pp
 from pyparsing import pyparsing_common as ppc
-from pyparsing import pyparsing_unicode as ppu
 
-uword = pp.Word(ppu.printables)
+uword = pp.SkipTo(pp.WordEnd())
 
 common_parsers = {
     int: pp.Combine(pp.Optional('-') + pp.Word(pp.nums)).setParseAction(ppc.convertToInteger) + pp.Suppress(
@@ -12,20 +11,17 @@ common_parsers = {
 }
 
 
-def update_dict(dict_list, update_func=lambda old, new: new):
+def update_dict(dict_list):
     ret = dict()
     for i in dict_list:
         for k, v in i.items():
-            if k in ret:
-                ret[k] = update_func(ret[k], v)
-            else:
-                ret[k] = v
+            ret[k] = v
     return ret
 
 
 def intersperse_parser(parser_list: list, interspersed: pp.ParserElement) -> pp.ParserElement:
-    ret = interspersed
+    ret = interspersed.copy()
     for i in parser_list:
-        ret = ret + i
+        ret = ret + i.copy() + interspersed.copy()
 
     return ret
