d5fade476b007435cbccd103ad3ae24d63864c4b
Nicholas <nics-lohr@gmx.de>
2020-03-29 22:06:28 +0200

    Improved docker run.

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --name chatbot -p 5000:9003 --restart always -d chatbot:latest
+#                                       vvvv bind the pip cache to the local, improves compile times.
+docker run --name chatbot -p 5000:9003 -v $HOME/.cache/pip/:/root/.cache/pip --restart on-failure:3 -d chatbot:latest
