818f38446aab6d27a0baef7956a7ac5fbfeaf85d
Nicholas <nics-lohr@gmx.de>
2020-03-29 16:59:33 +0200

    Added an async scheduler.

---
+++ chatbot/utils/async_sched.py
@@ -0,0 +1,27 @@
+from asyncio import create_task, sleep, CancelledError
+
+
+class AsyncScheduler:
+    def __init__(self, timeout, callback):
+        self.timeout = timeout
+        self.callback = callback
+        self.task = None
+
+    def run(self):
+        self.task = create_task(self.wait())
+
+    async def wait(self):
+        await sleep(self.timeout)
+        return self.callback()
+
+    async def reset(self, new_timeout):
+        await self.cancel()
+        self.timeout = new_timeout
+        self.run()
+
+    async def cancel(self):
+        self.task.cancel()
+        try:
+            return await self.task()
+        except CancelledError:
+            pass
