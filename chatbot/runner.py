a52d4b2d7ad0a1a3f844f328fe6b099f84ec2315
Nicholas <nics-lohr@gmx.de>
2020-03-29 21:12:34 +0200

    Added console server to connect with netcat.

--- chatbot/runner.py
+++ chatbot/runner.py
@@ -1,4 +1,5 @@
 from aioconsole.console import interact
+from aioconsole.server import start_console_server
 
 from chatbot import config
 from chatbot.bots.botmaster import BotMaster
@@ -13,12 +14,17 @@ class Runner:
         self.bridge = Bridge()
         self.chat = Chat(self.bridge, _config)
         self.botmaster = BotMaster(self.bridge, _config)
+        self.server = None
 
     async def wait_running(self):
         d = locals().copy()
-        d.update(globals())
+        d["botmaster"] = self.botmaster
+        d["bridge"] = self.bridge
+        d["chat"] = self.chat
+        d["exit"] = lambda: self.server.close()
 
-        await interact(locals=d)
+        self.server = await start_console_server(port=9001, locals=d)
+        await self.server.wait_closed()
 
     async def shutdown(self):
         await self.chat.shutdown()
