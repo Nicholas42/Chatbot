bcab9f85768a1d66fb4a6feb84574582df4e1eab
Nicholas <nics-lohr@gmx.de>
2020-03-30 16:53:49 +0200

    Fixed docker ports.

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #                                       vvvv bind the pip cache to the local, improves compile times.
-docker run --name chatbot -p 5000:9003 -v $HOME/.cache/pip/:/root/.cache/pip --restart on-failure:3 -d chatbot:latest
+docker run --name chatbot -p 9003:5000 -v $HOME/.cache/pip/:/root/.cache/pip --restart on-failure:3 -d chatbot:latest
