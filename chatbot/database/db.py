f521c922837105ceedb6489e22554d0484770731
Nicholas <nics-lohr@gmx.de>
2020-03-30 13:30:34 +0200

    Added database configuration.

--- chatbot/database/db.py
+++ chatbot/database/db.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.declarative import DeclarativeMeta
 from sqlalchemy.orm import sessionmaker, Session
 
@@ -14,7 +15,7 @@ class DB:
         self.active = _config["db"]["active"]
         conf = adapt_config(_config[self.active], _config[self.active]["adaptor"])
 
-        self.engine = create_engine(**conf)
+        self.engine = create_engine(self._create_url(conf))
         self._session_class = sessionmaker(bind=self.engine)
 
     @property
@@ -23,3 +24,8 @@ class DB:
 
     def create_all(self, base: DeclarativeMeta = Base):
         base.metadata.create_all(self.engine)
+
+    @staticmethod
+    def _create_url(conf):
+        _URL_ARGS = ["drivername", "username", "password", "host", "port", "database", "query"]
+        return URL(**dict((i, conf.get(i)) for i in _URL_ARGS))
