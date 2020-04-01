9d82b6b85946d391f63fd3bfa5138966649698b1
Nicholas <nics-lohr@gmx.de>
2020-04-01 02:26:29 +0200

    Fixed typo.

--- chatbot/bots/utils/parsing/date.py
+++ chatbot/bots/utils/parsing/date.py
@@ -7,5 +7,5 @@ def parse_date(s: str, *args, **kwargs) -> datetime:
     _SETTINGS = {"PREFER_DATES_FROM": "future", "DATE_ORDER": "DMY"}
     ret = parse(s, *args, languages=["de"], settings=_SETTINGS, **kwargs)
     if not ret:
-        return parse_date(s, *args, languages=["en"], settings=_SETTINGS, **kwargs)
+        return parse(s, *args, languages=["en"], settings=_SETTINGS, **kwargs)
     return ret
