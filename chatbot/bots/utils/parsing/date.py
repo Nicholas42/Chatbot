f1b9fbe1a9edef0f96deabf000271257a062994d
Nicholas <nics-lohr@gmx.de>
2020-04-02 15:54:35 +0200

    Datepaser: Provided wrong argument for timezone.

--- chatbot/bots/utils/parsing/date.py
+++ chatbot/bots/utils/parsing/date.py
@@ -4,7 +4,8 @@ from dateparser import parse
 
 
 def parse_date(s: str, *args, **kwargs) -> datetime:
-    _SETTINGS = {"PREFER_DATES_FROM": "future", "DATE_ORDER": "DMY", "RETURN_AS_TIMEZONE_AWARE": True, "TIMEZONE": "DE"}
+    _SETTINGS = {"PREFER_DATES_FROM": "future", "DATE_ORDER": "DMY", "RETURN_AS_TIMEZONE_AWARE": True,
+                 "TIMEZONE": "Europe/Berlin"}
     ret = parse(s, *args, languages=["de"], settings=_SETTINGS, **kwargs)
     if not ret:
         return parse(s, *args, settings=_SETTINGS, **kwargs)
