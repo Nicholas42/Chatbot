31ad1ed56ba74352e565a1da5e6b947575cdb98c
Nicholas <nics-lohr@gmx.de>
2020-04-02 22:47:38 +0200

    Made nicknames case insensitive and strippend whitespaces.

--- chatbot/database/nickname.py
+++ chatbot/database/nickname.py
@@ -1,13 +1,31 @@
-from sqlalchemy import Column, Integer, ForeignKey, String
+from sqlalchemy import Column, Integer, ForeignKey, String, types
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import relationship
 
+from chatbot import glob
 from chatbot.database import Base
 from chatbot.database.utils import IDMixin
 
 
+def normalize_nickname(nickname: str):
+    return nickname.strip().lower()
+
+
+class NicknameColumn(types.TypeDecorator):
+    impl = types.String
+
+    def process_bind_param(self, db_entry: str, dialect) -> str:
+        return db_entry
+
+    def process_result_value(self, nickname: str, dialect) -> str:
+        return normalize_nickname(nickname)
+
+    def copy(self, **kw):
+        return NicknameColumn(self.impl.length)
+
+
 class Nickname(IDMixin, Base):
-    nickname = Column(String, unique=True)
+    nickname = Column(NicknameColumn, unique=True)
     user_id = Column(Integer, ForeignKey("qedler.user_id"))
 
     qedler = relationship("QEDler", back_populates="nicknames")
