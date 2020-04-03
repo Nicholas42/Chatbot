23f0c4849b078d38e163198a1597212d3fe4cb97
Nicholas <nics-lohr@gmx.de>
2020-04-03 17:52:15 +0200

    Added ping table.

--- chatbot/database/__init__.py
+++ chatbot/database/__init__.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
 
-__all__ = ["messages", "songs", "Base", "nickname"]
+__all__ = ["messages", "songs", "Base", "nickname", "ping"]
 
 Base = declarative_base()
---
+++ chatbot/database/ping.py
@@ -0,0 +1,22 @@
+import datetime
+
+from sqlalchemy import Column, Integer, ForeignKey, TIMESTAMP, String, Text
+from sqlalchemy.ext.hybrid import hybrid_property
+
+from chatbot.database import Base
+from chatbot.database.utils import IDMixin, utcnow
+
+MIN_DATE = datetime.datetime.min.replace(tzinfo=datetime.timezone.utc)
+
+
+class Ping(IDMixin, Base):
+    user_id = Column(Integer, ForeignKey("qedler.user_id"))
+    target = Column(Integer, ForeignKey("nickname._column_id"))
+    message = Column(Text, nullable=False)
+    ping_time = Column(TIMESTAMP(timezone=True), nullable=False, server_default=utcnow())
+    sender = Column(String, nullable=False)
+    activation_time = Column(TIMESTAMP(timezone=True), server_default=utcnow(), nullable=False)
+
+    @hybrid_property
+    def is_active(self):
+        return self.activation_time > datetime.datetime.now(datetime.timezone.utc)
