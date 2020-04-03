a74c66b56916e9c054b28426c6462003c45571ae
Nicholas <nics-lohr@gmx.de>
2020-04-03 21:06:39 +0200

    Added tests for get_ping.

--- tests/db/ping.py
+++ tests/db/ping.py
@@ -1,12 +1,23 @@
 from unittest import TestCase
 
 from chatbot import glob
-from chatbot.database.nickname import QEDler, Nickname, create_nickname
-from chatbot.database.ping import Ping
+from chatbot.database.nickname import QEDler, Nickname, create_nickname, get_user
+from chatbot.database.ping import Ping, get_pings
 
 
 class TestPing(TestCase):
     glob.configure()
+
+    def test_get_pings(self):
+        with glob.db.context as session:
+            p = Ping(user_id=412, message="Blub", sender="Your Mom")
+            session.add(p)
+
+            self.assertEqual(get_user(session, "EpsiLon").user_id, 412)
+            self.assertIn(p, get_pings(session, "EpsiLon"))
+            self.assertIn(p, get_pings(session, "NicholasSchwab"))
+
+            session.rollback()
 
     def test_user(self):
         p = Ping(user_id=412, message="Blub", sender="Your Mom")
@@ -21,8 +32,12 @@ class TestPing(TestCase):
 
             nick = session.query(Nickname).filter(Nickname.nickname == "schAtzi   ").one()
 
-            p.target = nick._column_id
+            p.target = nick
 
             self.assertTrue(p in nick.pings)
+            self.assertTrue(p.is_active)
+            self.assertTrue(p in session.query(Ping).filter(Ping.is_active).all())
+            self.assertTrue(p in session.query(Ping).filter(Ping.target == nick).filter(Ping.is_active).all())
+            self.assertTrue(p in filter(lambda x: x.is_active, nick.pings))
 
             session.rollback()
