c5f335651d637fcfaf7479d9be9e00d05eea4338
Nicholas <nics-lohr@gmx.de>
2020-04-19 21:59:53 +0200

    Added command line option to run local chat.

--- chatbot/__main__.py
+++ chatbot/__main__.py
@@ -1,12 +1,14 @@
 import asyncio
+import sys
 
 from chatbot.runner import Runner
 
 
-async def main():
-    r = Runner()
+async def main(*args,**kwargs):
+    r = Runner(*args, **kwargs)
     await r.wait_running()
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    local = len(sys.argv) > 1 and sys.argv[1] == "local"
+    asyncio.get_event_loop().run_until_complete(main(local=local))
